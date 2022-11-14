60a182d0b95a6fc77c3019ad4dd0ada0f64c9087
morgane-marechal <morgane.marechal@laplateforme.io>
2022-11-14 14:09:19 +0100

    add job01

---
+++ jour01/index.php
@@ -0,0 +1,18 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo $str2." ".$str." ".$str3;
+echo "\n";
+echo "$str2 $str $str3";
+echo "\n";
+$val = 6;
+$val = $val + 4;
+echo $val;
+echo "\n";
+$myBool = true;
+echo "True: $myBool";
+echo "\n";
+$myBool = false;
+echo "False: $myBool";
+?>
