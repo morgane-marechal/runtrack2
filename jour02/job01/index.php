93b7e47a0b76325450852773ddc074fb5d46d8b3
morgane-marechal <morgane.marechal@laplateforme.io>
2022-11-15 11:47:43 +0100

    add job 01 et Job02 dans jour02

---
+++ jour02/job01/index.php
@@ -0,0 +1,12 @@
+<?php
+
+for ($i = 0; $i <= 1337; $i++) {
+        if ($i ==42){
+            echo "<b><u>$i</u></b>";
+            echo "<br>";
+        } else {
+            echo $i;
+            echo "<br>";
+}
+}
+?>
