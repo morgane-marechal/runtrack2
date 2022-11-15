d4445935430aa2c931ec421dfc63fef16ef32876
morgane-marechal <morgane.marechal@laplateforme.io>
2022-11-15 16:36:14 +0100

    deuxième methode job05

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -37,4 +37,30 @@ for ($i = 1; $i <= 1000; $i++) {
     }
 }
 
+
+echo "----- deuxième methode";
+
+echo '<br>Nombre premier de 0 à 1000: ';
+$n=1000;
+for($i=2;$i<=$n;$i++){
+        $nbDiv = 0;//Et on compte le nombre de diviseur    
+            for($j=1;$j<=$i;$j++){
+                if($i%$j==0){
+                    $nbDiv++; 
+                }
+            }
+            if($nbDiv == 2){
+                //Un nombre premier est un chiffre qui ne possède que 2 diviseur (1 et
+                // lui-même)
+            echo $i; 
+            echo "<br>";             
+            }else{
+                echo ".";
+                echo "<br>";
+            }
+
+
+}
+        
+    
 ?>
