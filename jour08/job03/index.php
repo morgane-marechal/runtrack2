1561c1e4774be1bd2fbd070d881e07f62f71b8e0
morgane-marechal <morgane.marechal@laplateforme.io>
2022-11-23 14:59:34 +0100

    add job03 jour08

--- jour08/job03/index.php
+++ jour08/job03/index.php
@@ -1,4 +1,29 @@
 <?php
-    
+session_start();  
 
+if (!isset($_SESSION["affiche_prenom"])) {
+    $_SESSION['affiche_prenom'] = [];
+}
+
+
+if(isset($_POST["prenom"])) {
+    if (isset($_SESSION['affiche_prenom'])){
+         $_SESSION['affiche_prenom'][]=$_POST["prenom"];
+         
+         foreach ($_SESSION['affiche_prenom'] as $name){
+            echo $name;
+            echo "<br>";
+         }
+    }
+}
+?>
+
+<form action="" method="post">
+<input type="text" value="prenom" name="prenom"></input>   
+<input type="submit" value="Envoyer"></input>
+</form>
+
+<?php
+var_dump($_SESSION);
+echo $_SESSION['affiche_prenom'];
 ?>
