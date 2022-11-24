8c0e0174b1d7d35fde3bc5c187a6bcb63481e65e
morgane-marechal <morgane.marechal@laplateforme.io>
2022-11-24 10:37:41 +0100

    correction jour08 job04

--- jour08/job04/index.php
+++ jour08/job04/index.php
@@ -1,26 +1,70 @@
 <?php
-echo "<br>";
-echo "Nom utilisateur: ".$_COOKIE['user'];
-echo "<br>";
+setcookie('eleves','',time()+3600);
 
-setcookie('user',$user, time()+3600);
-
-//if (isset($_COOKIE['user'])) {
-    if(isset($_POST["prenom"])){
-        $user = $_POST["prenom"];
-        setcookie('user', $user);
-     }
-//}
-echo "<br>";
-echo "Nom utilisateur: ".$_COOKIE['user'];
 echo "<br>";
 
-    var_dump($_COOKIE);
+ if(isset($_GET['connexion'])){
+    $_COOKIE['eleves'] = $_GET['prenom'];
+}
 
+echo "<br>";
+
+if(isset($_COOKIE['eleves'])){
+    
+    echo "bonjour : " . $_COOKIE['eleves'];
+    echo '<form action="" method="GET"> <input type="submit" name="deco" value="deco :)" > </form>';
+
+}
+
+
+echo "<br>";
+
+
+
+echo "<hr>";
 ?>
 
 
-<form action="" method="post">
-<input type="text" value="prenom" name="prenom"></input>   
-<input type="submit" value="Connexion"></input>
-</form>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Job04</title>
+    </head>
+    <body>
+
+    <?php 
+     if(!isset($_COOKIE['eleves'])) {
+        echo ' <form action="" method="GET">
+        <label for="pre">Prenom</label>
+        <br>
+        <br>
+        <input type="text" name="prenom" placeholder="votre prenom">
+        <br>
+        <br>
+        <input type="submit" name="connexion" value="connexion">
+    </form>';
+        }
+     ?>
+   
+        <?php /* if(isset($_GET['deco'])){ 
+        
+            setcookie('eleves','',time()+3600);
+
+            echo ' <form action="" method="GET">
+            <label for="pre">Prenom</label>
+            <br>
+            <br>
+            <input type="text" name="prenom" placeholder="votre prenom">
+            <br>
+            <br>
+            <input type="submit" name="connexion" value="connexion">
+        </form>';
+        }*/
+        ?>
+      
+    </body>
+    </html>
+
